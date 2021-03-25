508f2178470dfe495117157381546225a6696189
cpach96 <cpach96@gmail.com>
2021-03-24 20:41:05 -0400

    commit tree debe99e1a7194977c8ddbb213f621e1a2b262b6b

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function saturdayFun(event="roller-skate"){
+    return(`This Saturday, I want to ${event}!`)
+}
+
+function mondayWork(event="go to the office"){
+    return(`This Monday, I will ${event}.`)
+}
+
+function wrapAdjective(flair="*"){
+    return function(compliment="special") {
+        return(`You are ${flair}${compliment}${flair}!`)
+    }
+}
+
+const Calculator = {
+    add: (function(){ return 1 + 3}),
+    subtract: (function(){ return 1 - 3}),
+    multiply: (function(){ return 1 * 3}),
+    divide: (function(){ return 10 / 5})
+}
+
+function actionApplyer(integer,array){
+    for (let i = 0; i < array.length; i++){
+        integer = array[i](integer)
+    }
+    return integer
+}
